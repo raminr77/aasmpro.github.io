bd0aa4f08ffcf21ce5b9ba1291c656b1c3054f72
Ramin Rezaei <rare@boozt.com>
2024-04-18 14:35:21 +0200

    fix: refactor the routes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // router
-import { BrowserRouter } from "react-router-dom";
-import { Main } from "./Main";
+import { ScrollToTop } from "./utils/scrollToTop";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+// views
+import { Home } from "./views/Home";
+import { Resume } from "./views/Resume";
+import { Projects } from "./views/Projects";
+import { NotFound } from "./views/NotFound";
 
 // styles
 import "./assets/styles/fonts.fontawesome.min.css";
@@ -11,7 +17,15 @@ import "./assets/styles/main.min.css";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Main />
+    <ScrollToTop />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/404/" component={NotFound} />
+      <Route path="/resume/" component={Resume} />
+      <Route path="/projects/" component={Projects} />
+
+      <Redirect from="*" to="/404/" />
+    </Switch>
   </BrowserRouter>,
   document.getElementById("root")
 );
